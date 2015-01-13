da7b140dad335cc98e02f67459ddcc90ebb7be5b
Randy S Balaban <rsb@2BitProgrammers.com>
2015-01-13 03:58:26 -0800

    Initial Commit.
    This is the solution to the problem found at:
    http://stackoverflow.com/questions/20943833/how-to-render-a-internal-image-on-a-swing-component-using-html/27921370#27921370

---
+++ src/expinlineimage/Custom_HTMLEditorKit.java
@@ -0,0 +1,43 @@
+
+package expinlineimage;
+
+
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.Element;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLEditorKit;
+
+
+public class Custom_HTMLEditorKit extends HTMLEditorKit {
+    private static HTMLEditorKit.HTMLFactory factory = null;    
+
+    @Override
+    public ViewFactory getViewFactory() {
+        if (factory == null) {
+            factory = new HTMLEditorKit.HTMLFactory() {
+
+                @Override
+                public View create(Element elem) {
+                    AttributeSet attrs = elem.getAttributes();
+                    Object elementName = attrs.getAttribute(AbstractDocument.ElementNameAttribute);
+                    Object o = (elementName != null) ? null : attrs.getAttribute(StyleConstants.NameAttribute);
+                    if (o instanceof HTML.Tag) {
+                        HTML.Tag kind = (HTML.Tag) o;
+                        if (kind == HTML.Tag.IMG) {
+                            // HERE is the call to the special class...
+                            return new Custom_ImageView(elem);
+                        } // End if(kind == IMG)...
+                    } // End if(instance of Tag)...
+                    return super.create(elem);
+                } // End create()...
+                
+            }; // End new HTMLFactory()...
+        } // End if(factory == null)...
+        return factory;
+    } // End getViewFactory()...
+ 
+} // End RITB64_HTMLEditorKit()...
